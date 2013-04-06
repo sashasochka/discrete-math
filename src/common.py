70723decc7dba9a346521394f3f0535a9ed846dc
Oleksandr <sasha.sochka@gmail.com>
2013-04-06 19:10:02 +0300

    Additional functionality (e.g. UseIO class for talking to user)

--- src/common.py
+++ src/common.py
@@ -10,19 +10,24 @@ class IO:
     either command line argumnts or stdin/stdout
     and provides a generilized way of printing to output file
     """
+
     def __init__(self):
         """
         Initialize input and ouput file objects from command line options or
         if absent from stdin/stdout
         """
         try:
+            self.close_filein = False
+            self.close_fileout = False
             if len(sys.argv) > 1:
                 self.filein = open(sys.argv[1], 'r')
+                self.close_filein = True
             else:
                 self.filein = sys.stdin
 
             if len(sys.argv) > 2:
                 self.fileout = open(sys.argv[2], 'w')
+                self.close_fileout = True
             else:
                 self.fileout = sys.stdout
         except IOError as e:
@@ -35,3 +40,43 @@ class IO:
         """
         kwargs['file'] = self.fileout
         print(*args, **kwargs)
+
+    def readline(self) -> str:
+        """
+        Read and return string readed from input file
+        Return:
+            readed string
+        """
+        return self.filein.readline()
+
+    def __del__(self):
+        """
+        Close opened files if needed
+        """
+        if self.close_filein:
+            self.filein.close()
+        if self.close_fileout:
+            self.fileout.close()
+
+
+class UserIO:
+    """
+    Class for talking to user through stdin/stdout
+    """
+    def readint(self, start: int=-sys.maxsize, stop: int=sys.maxsize,
+                label: str='integer') -> int:
+        """
+        Read and return integer readed from input file after user prompt
+        Return:
+            read integer
+        """
+        val = None  # read integer
+        repeat = True
+        while repeat:
+            print('Input {} (in range [{};{})'.format(label, start, stop))
+            try:
+                val = int(input())
+                repeat = False
+            except ValueError:
+                print('Cannot read as integer!')
+        return val
