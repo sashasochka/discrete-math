fee0d81db9d94070a50be053c0048a543bcced5f
Oleksandr <sasha.sochka@gmail.com>
2013-04-08 20:12:15 +0300

    Implement 1st part (Floyd-Warshall

--- src/lab6.py
+++ src/lab6.py
@@ -1,2 +1,34 @@
 #!/usr/bin/env python3
-__author__ = 'Sochka Oleksandr; sasha.sochka@gmail.com'
+from common import IO, UserIO
+from graph import algorithm
+from graph.weighted.directed import Graph
+
+io = IO()
+userio = UserIO()
+G = Graph.fromfile(io.filein)
+
+
+# floyd-warshall part
+io.section('Floyd-Warshall\'s algorithm')
+search_results = algorithm.floyd_warshall(G)
+if search_results is None:
+    io.print('Cannot find distance using Floyd-Warshall - negative cycles are '
+             'present')
+else:
+    # print distance matrix
+    io.print('Distance matrix: ')
+    io.print_matrix(algorithm.distance_matrix(search_results), 's\\t')
+
+
+    s = userio.readvertex(G.V(), 'source')
+    t = userio.readvertex(G.V(), 'destination')
+
+    # print path from s to t
+    path = algorithm.forwardtrace_path_from_all_to_all(search_results, s,
+                                                           t)
+    if path is None:
+        io.print('There is no path from vertex #{} to #{}'.format(s + 1,
+                                                                  t + 1))
+    else:
+        io.print('Path from #{} to #{}:'.format(s + 1, t + 1))
+        io.print(' -> '.join([str(i + 1) for i in path]))
